059ce25605d160367bcdbba20e191702bbbf2b9a
Artem <mercooler666@gmail.com>
2025-05-16 21:28:41 +0300

    lesson2

--- lesson1/script.js
+++ lesson1/script.js
@@ -1,1 +1,17 @@
-console.log(1)
+let number = 5;  /// let - обьявление переменной "number" - название "=" - знак присваинвания
+const leftBorderWidth = 1; // const - другой способ обьявление константы "leftBorderWidth" - название "=" - знак присваинвания
+number = 10;
+console.log(number); // вывод в консоль - "10"
+const obj = {
+    a: 10,
+}
+
+obj.a = 20;
+
+console.log(obj);
+console.log(name);
+var name = 'Ivan'; // var - устаревший способ обьявления переменной "name" - название "=" - знак присваинвания
+
+name = 'Alex'; 
+
+
---
+++ script.js
@@ -0,0 +1,14 @@
+const userName = {
+    name: 'John',
+}    
+console.log(userName.name)
+
+const userNumber = {
+    number: 25,
+}
+
+userNumber.number = 24
+
+let userName = 'John'
+let userNumber = 25
+userNumber = 24
